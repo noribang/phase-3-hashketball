9ec009558ecd9ec1969c3d2f453472e8c6cbd22f
hoonkim <hoonk9387@gmail.com>
2022-07-16 21:26:20 -0500

    Completed assignment

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,6 @@
 # Write your code below game_hash
+require 'pry'
+
 def game_hash
   {
     home: {
@@ -127,3 +129,165 @@ def game_hash
 end
 
 # Write code here
+
+#1 num_points_scored
+def num_points_scored(name)
+  player_points = 0
+  # players = game_hash[:home][:players]
+  game_hash.each do |key, value|
+    # binding.pry
+    # value.each do |attribute, data|
+    #   binding.pry
+    # end
+
+    value[:players].each do |player|
+      if player[:player_name] == name
+        # puts player[:player_name] + "!"
+        # puts player[:points].to_s + "!"
+        player_points = player[:points]
+      end
+    end
+  end
+
+  return player_points
+  # binding.pry
+end
+# TEST num_points_scored
+# num_points_scored("Jeff Adrien")
+
+# 2 shoe_size
+def shoe_size(name)
+  player_shoe = 0
+  # players = game_hash[:home][:players]
+  game_hash.each do |key, value|
+    # binding.pry
+    # value.each do |attribute, data|
+    #   binding.pry
+    # end
+
+    value[:players].each do |player|
+      if player[:player_name] == name
+        # puts player[:player_name] + "!"
+        # puts player[:shoe].to_s + "!"
+        player_shoe = player[:shoe]
+      end 
+    end
+  end
+
+  return player_shoe
+  # binding.pry
+end
+# TEST shoe_size
+# shoe_size("Jeff Adrien")
+# shoe_size("Alan Anderson")
+
+# 3 team_colors
+def team_colors(team)
+  array_colors = []
+
+  game_hash.each do |key, value|
+    if value[:team_name] == team
+      array_colors = value[:colors]
+    end
+    # binding.pry
+  end
+
+  return array_colors
+  # binding.pry
+end
+# TEST team_colors
+# team_colors
+# team_colors("Brooklyn Nets")
+# team_colors("Charlotte Hornets")
+
+# 4 team_names
+def team_names
+  teams = []
+
+  game_hash.each do |key, value|
+    teams << value[:team_name]
+    # binding.pry
+  end
+
+  return teams
+end
+# TEST team_names
+# team_names
+
+
+# 5 player_numbers
+def player_numbers(team)
+  play_num = []
+
+  game_hash.each do |key, value|
+    # puts value[:team_name]
+    value[:players].each do |player|
+      if value[:team_name] == team
+        # puts player[:number]
+        play_num << player[:number]
+      end
+    end
+
+  end
+
+  # binding.pry
+  return play_num
+end
+# TEST player_numbers
+# player_numbers("Charlotte Hornets")
+# player_numbers("Brooklyn Nets")
+
+
+# 6 player_stats
+def player_stats(name)
+
+  game_hash.each do |key, value|
+    # puts value[:team_name]
+    value[:players].each do |player|
+      if player[:player_name] == name
+        return player
+      end
+    end
+  end
+
+  # binding.pry
+end
+# TEST player_stats
+# player_stats
+# player_stats("Jeff Adrien")
+
+
+# 7 big_shoe_rebounds
+def big_shoe_rebounds
+  biggest_shoe = 0
+  player_obj = {}
+
+  game_hash.each do |key, value|
+    # puts value[:team_name]
+    value[:players].each do |player|
+      # name = player[:player_name]
+      # rebo = player[:rebounds]
+      shoe_s = player[:shoe]
+      
+      if shoe_s > biggest_shoe
+        biggest_shoe = shoe_s
+        player_obj = player
+        # puts player[:player_name]
+        # puts shoe_s
+      end
+
+      # binding.pry
+    end
+  end
+
+  puts "biggest_shoe: #{biggest_shoe}"
+  puts "player_name: #{player_obj[:player_name]}"
+  puts "rebounds:#{player_obj[:rebounds]}"
+  player_big_shoe_rebounds = player_obj[:rebounds]
+  return player_big_shoe_rebounds
+end
+# TEST big_shoe_rebounds
+big_shoe_rebounds
+
+
+
